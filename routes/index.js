3e5f745cc1677039931e448ffb44e4bb6ce85e42
David Sivocha <david@sivocha.com>
2012-06-19 11:17:25 +0100

    index page lists articles in reverse order of creation date
    Signed-off-by: David Sivocha <david@sivocha.com>

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,26 @@ var articles = './articles';
 var fs = require('fs');
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+	fs.readdir(articles, function(err,files){
+		if (err){
+			res.render('index', { title: 'Error' });
+		} else {
+			var titles = [];
+			files.sort(function(a, b) {
+				return fs.statSync(articles + "/" + b).mtime.getTime() - fs.statSync(articles + "/" + a).mtime.getTime();
+			});
+			for(var i = 0; i < files.length; i++ ){
+				var each = files[i];
+				var article = articles + '/' + each;
+				var data = fs.readFileSync(article, 'utf-8');
+				var array = data.toString().split("\n \n");
+				var parsed = JSON.parse(array[0]);
+				titles.push(parsed.title);
+				console.log(titles);
+			}
+			res.render('index', { title: titles });
+		}
+	});
 };
 
 exports.about = function(req, res){
