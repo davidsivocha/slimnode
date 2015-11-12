ecb44c69fc7ef714dab068b630c609525e4a92ed
Rob Wilson <robwilson101@gmail.com>
2015-11-12 10:52:28 +0000

    Migrated to express 4.13.3

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.configure(function () {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
-  app.use(express.logger('dev'));
+  app.use(morgan('dev'));
   app.use(express.static(__dirname + '/public'));
   app.use(bodyParser.urlencoded({extended:true}));
   app.use(express.methodOverride());
