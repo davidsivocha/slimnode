62b88253796b9917e7d42931901f68860a976a47
Rob Wilson <robwilson101@gmail.com>
2015-11-12 10:56:29 +0000

    Final change to express 4

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
-var express = require('express')
-  , fs = require('fs')
-  , http = require('http')
-  , router = require('./config/routes')
-  , settings = require('./config/settings')
-  , bodyParser = require('body-parser')
-  , methodOverride = require('method-override')
-  , port = process.env.PORT || 8080
-  , app = express();
+var express = require('express');
+var fs = require('fs');
+var http = require('http');
+var router = require('./config/routes');
+var settings = require('./config/settings');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var morgan = require('morgan');
+var port = process.env.PORT || 8080;
+var app = express();
 
-app.configure(function () {
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(morgan('dev'));
-  app.use(express.static(__dirname + '/public'));
-  app.use(bodyParser.urlencoded({extended:true}));
-  app.use(express.methodOverride());
-  app.use('/', router);
-});
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 
-app.configure('development', function () {
-  app.use(express.errorHandler());
-});
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride());
+app.use('/', router);
 
 if(settings.development){
   //runs on port 8080 if development mode
