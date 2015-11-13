52dc75ca965969629f2f99101739fde2786bfdcb
David Sivocha <david@sivocha.com>
2015-11-13 19:34:15 +0000

    Added port settings

--- config/settings/index.js
+++ config/settings/index.js
@@ -7,5 +7,10 @@ var settings = {};
 settings.sitename = "The DaveZilla Blog";
 settings.url = "http://osirisapp.com:8080/"
 settings.development = true; //use true for development, false for productions
+if(settings.development) {
+    settings.port = 8080;
+} else {
+    settings.port = 80
+}
 
 module.exports = settings;
