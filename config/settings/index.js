25af4af75dba5066daa8c5d83b7d8223f132abce
Rob Wilson <robwilson101@gmail.com>
2016-01-07 16:46:30 +0000

    Use environment variable to determine boolean value of 'settings.development'
    If production is set to 'development' then settings.development is true, else if set to 'production, the boolean value is false.
    
    Set production on the fly in CLI with .
    Check value with production.

--- config/settings/index.js
+++ config/settings/index.js
@@ -6,11 +6,7 @@ var settings = {};
 
 settings.sitename = "The DaveZilla Blog";
 settings.url = "http://osirisapp.com:8080/"
-settings.development = true; //use true for development, false for productions
-if(settings.development) {
-    settings.port = 8080;
-} else {
-    settings.port = 80
-}
+settings.development = ((process.env.NODE_ENV || 'development').trim().toLowerCase() !== 'production');
+settings.development ? settings.port = 8080 : settings.port = 80;
 
 module.exports = settings;
